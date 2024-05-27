6f676e374f9a8fbac105e67fea9587d956460754
danihek <64467514+danihek@users.noreply.github.com>
2024-05-27 02:31:21 +0200

    Update clientUDP.c

--- clientUDP.c
+++ clientUDP.c
@@ -1,3 +1,4 @@
+/* For X11 only for now */
 #include <stdlib.h> 
 #include <sys/types.h> 
 #include <sys/socket.h> 
@@ -5,21 +6,26 @@
 #include <netinet/in.h> 
 #include <stdio.h>
 #include <string.h>
+#include <X11/Xlib.h>
+#include <math.h>
 
-int main(int argc, char*argv[])
+int width = 1600;
+int height = 900;
+
+int main(int argc, char *argv[])
 {
 	if (argv[1]==0)
 	{
-	   printf("Usage: tcom [ip]");
+	   printf("Usage: fpen [ip]");
 		exit(1);
 	}
-	
+
 	int port = 6969;
-	int bufferSize = 255;
+	int bufferSize = 50;
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	
 	char buffer[bufferSize];
-	char *ip = "192.168.31.223";
+	char *ip = argv[1];
 
 	struct sockaddr_in addr;
 
@@ -39,43 +45,57 @@ int main(int argc, char*argv[])
 	}
 	addr_size = sizeof(addr);
 
-	int x = 0, y =0;
+	strcpy(buffer,"0");
+	sendto(sockfd,
+			buffer,
+			bufferSize,
+			0,
+			(struct sockaddr*)&addr,
+			addr_size);
 
-	// TODO - get screen resolution
-	int width = 1600;
-	int height = 900;
-
+	float x = 0, y = 0, z = 0, ax, ay, az;
+	float sf = 10; /* scaling factor */
+	
+	
 	while (1)
 	{
-		strcpy(buffer,"0");
-		sendto(sockfd,buffer,bufferSize,0,(struct sockaddr*)&addr,addr_size);
+		Display *display;
+		Window root;
+		display = XOpenDisplay(NULL);
+		if (display == NULL)
+		{
+	     fprintf(stderr, "Cannot open display\n");
+	     return 1;
+		}
+		int screen = DefaultScreen(display);
+		XWindowAttributes attributes;
+		root = RootWindow(display, screen);
 		recvfrom(sockfd,buffer,bufferSize,0,(struct sockaddr*)&addr,&addr_size);
+		buffer[bufferSize] = '\0';
 
-		x -= atoi(buffer)/16;
-		if (x > width)
-		{
-			x = width;
-		}
-		if (x < 0)
-		{
-			x = 0;
-		}
+		char *token = strtok(buffer, ",");
+		ax = atof(token);
+		if (fabs(ax) > 0.1)
+			x = (x - ax * sf);
 
-		strcpy(buffer, "1");
-		sendto(sockfd,buffer,bufferSize,0,(struct sockaddr*)&addr,addr_size);
-		recvfrom(sockfd,buffer,bufferSize,0,(struct sockaddr*)&addr,&addr_size);
+		token = strtok(NULL, ",");
+		ay = atof(token);
+		if (fabs(ay) > 0.1)
+			y = (y - ay * sf );
 
-		y -= atoi(buffer)/16;
-		if (y > height)
-		{
-			y = height;
-		}
-		if (y < 0)
-		{
-			y = 0;
-		}
+		token = strtok(NULL, ",");
+		az = atof(token);
+		if (fabs(az) > 0.1)
+			z = (z - az * sf);
+		
+		if (x < 0) x = 0; if (x > width) x = width;
 
-		printf("%d - %d\n",x,y);
+		if (y < 0) y = 0; if (y > height) y = height;
+		if (z < 0) z = 0;	if (z > width) z = width;
+
+		printf("x = %f, y = %f, z = %f | x = %f, y = %f, z = %f\n", x, y, z, ax, ay, az);
+		XWarpPointer(display, None, root, 0, 0,width, height, z, y);	
+		XCloseDisplay(display);
 	}
 	return 0;
 }
